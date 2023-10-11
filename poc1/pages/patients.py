23a5c82deb301673da945fe7422be5a6ae82409b
Nsukami Di Kiesse Patrick <ptrck@nskm.xyz>
2023-10-11 06:13:41 +0000

    - Import contextlib to use sqlite3 cursor as a ctx mgr - Add convert_to_excel & init_connection functions - Remove function for creating the table (not sure) - Add form for uploading patients from xls file - We don't use sidebar, remove anything related to it - Add a downlod button to retrieve all the existing patients as xls file
    Refs #25

--- poc1/pages/patients.py
+++ poc1/pages/patients.py
@@ -1,10 +1,12 @@
 import re
+import io
 import sqlite3
 import pandas as pd
 import numpy as np
 import streamlit as st
 import pathlib
 import datetime
+import contextlib
 
 try:
     from streamlit import rerun as rerun
@@ -22,33 +24,45 @@ st.set_page_config(
     menu_items=None,
 )
 
+
+def convert_to_excel(df):
+    buffer = io.BytesIO()
+    with pd.ExcelWriter(buffer) as writer:
+       df.to_excel(writer)
+    return buffer
+
 st.write("# Patients management")
 
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.cache_resource
+def init_connection():
+    return sqlite3.connect(database_path, check_same_thread=False)
+
 if "con" not in st.session_state:
-    con = sqlite3.connect(database_path)
-    cur = con.cursor()
+    con = init_connection()
     st.session_state.con = con
-    st.session_state.cur = cur
 
-
-def create_patients_table():
-    st.session_state.cur.execute("""
-    create table if not exists patients(
-    eid integer primary key,
-    firstname varchar(50) not null,
-    lastname varchar(50) not null,
-    age integer not null,
-    sex varchar(50) not null,
-    created varchar(50) not null
-    );
-    """)
 
 def patients():
-    con = st.session_state.con
-
     # patient form insertion
     with st.expander("Import from Excel file"):
+        con = st.session_state.con
         st.write("## Import")
+        with st.form("my_patients_upload_form", clear_on_submit=True):
+            input_file = st.file_uploader(
+                "Input file",
+                type=["xls", "xlsx"],
+                help="Enter an Excel file containing patients list"
+            )
+            submit_button_form = st.form_submit_button("Submit file")
+            if submit_button_form:
+                columns = ["firstname", "lastname", "age", "sex"]
+                dfp = pd.read_excel(input_file, sheet_name="Patients", header=None, names=columns)
+                now = datetime.datetime.now().strftime("%Y/%m/%d")
+                dfp["created"] = pd.Series([now] * dfp.shape[0])
+                # insert inside table
+                dfp.to_sql('patients', con, if_exists='append', index=False)
 
     with st.expander("Add with form"):
         st.write("## Patient creation")
@@ -58,36 +72,28 @@ def patients():
             with c2:
                 firstname = st.text_input("Firstname")
                 lastname = st.text_input("Lastname")
-
             with c1:
-                age = st.text_input('Age', placeholder='00')
+                age = st.number_input('Age', placeholder='00', step=1, min_value=1)
                 sex = st.selectbox(
                     'Sex',
                     ('Male', 'Female')
                 )
-
-            if age != '':
-                try:
-                    number = int(age)  # try to convert the variable to an integer
-                except ValueError:  # if it's not convertible, catch the ValueError exception
-                    st.markdown(f"**:red[{age}]** is NOT a number")
-
             submit_button_form = st.form_submit_button("Submit")
             if submit_button_form:
-                cur = st.session_state.cur
                 con = st.session_state.con
                 now = datetime.datetime.now().strftime("%Y/%m/%d")
                 query = """
                 insert into patients(firstname, lastname, age, sex, created) values (?, ?, ?, ?, ?)
                 """
-                cur.execute(query, (firstname, lastname, age, sex, now))
-                con.commit()
+                with contextlib.closing(con.cursor()) as cur:
+                    cur.execute(query, (firstname, lastname, age, sex, now))
+                    con.commit()
 
     # patient list
     with st.expander("List"):
+        con = st.session_state.con
         df = pd.read_sql("SELECT * from patients", con)
         df["Delete"] = pd.Series([False] * df.shape[0], dtype=bool)
-
         st.write("## Patients list")
         with st.form("deletion_form", clear_on_submit=True):
             edited_df = st.data_editor(
@@ -108,23 +114,23 @@ def patients():
             ids_to_rm = [(i,) for i in ids_to_rm]
             delete = st.form_submit_button("Delete")
             if delete and len(ids_to_rm) != 0:
-                cur = st.session_state.cur
                 con = st.session_state.con
-                # selected_rows = edited_df[edited_df.Select]
-
-                # delete from patients where ids in
                 query = "delete from patients where eid = ?"
-                cur.executemany(query, ids_to_rm)
-                con.commit()
+                with contextlib.closing(con.cursor()) as cur:
+                    cur.executemany(query, ids_to_rm)
+                    con.commit()
                 # https://docs.streamlit.io/library/api-reference/control-flow/st.rerun
                 rerun()
-
-    # the menu inside the sidebar
-    # with st.sidebar.form("menu_form", clear_on_submit=True):
-    #     option = st.selectbox("Actions", ["Update"])
-    #     button = st.form_submit_button()
-
+        now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+        con = st.session_state.con
+        dfr = pd.read_sql("SELECT * from patients", con)
+        download = st.download_button(
+            label="Download patients",
+            data=convert_to_excel(dfr),
+            file_name=f"patients_list_{now}.xlsx",
+            mime="application/vnd.ms-excel",
+            key='download-csv'
+        )
 
 if __name__ == "__main__":
-    create_patients_table()
     patients()
