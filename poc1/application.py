5350cc53d4bdcc5731972dec83a187c294fdb4bc
Nsukami Di Kiesse Patrick <ptrck@nskm.xyz>
2023-10-19 11:23:24 +0000

    WIP: Update home page, add an introduction text

--- poc1/application.py
+++ poc1/application.py
@@ -7,7 +7,16 @@ def run():
         initial_sidebar_state="collapsed",
         menu_items=None,
     )
-    st.write("# Welcome to IPD")
+    st.write("# Welcome :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+    multi = '''This is a proof of concept built to help for the management of [PCR plate plans](#).
+
+
+Keep in mind, this is a :red[pre-alpha version], the software is :red[still being designed and built], the software should not be released to the public yet.
+
+To find more informations, please, follow this [link](#)
+    '''
+    st.markdown(multi)
 
 if __name__ == "__main__":
     run()
