9c5aca82168f9de945f2cd7808bd23a467c84413
Nsukami Di Kiesse Patrick <ptrck@nskm.xyz>
2023-10-23 11:00:12 +0000

    WIP: reduce content of Home.py module

--- poc1/Home.py
+++ poc1/Home.py
@@ -1,12 +1,15 @@
 import streamlit as st
+from database import init_connection, database_path, create_plate_types_table, create_plates_table
+
+st.set_page_config(
+    page_title="Home page",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items=None,
+)
+
 
 def run():
-    st.set_page_config(
-        page_title="Home page",
-        layout="wide",
-        initial_sidebar_state="collapsed",
-        menu_items=None,
-    )
     st.write("# Welcome :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 
     multi = '''This is a proof of concept built to help for the management of [PCR plate plans](#).
@@ -19,4 +22,6 @@ To find more informations, please, follow this [link](#)
     st.markdown(multi)
 
 if __name__ == "__main__":
+    create_plate_types_table()
+    create_plates_table()
     run()
