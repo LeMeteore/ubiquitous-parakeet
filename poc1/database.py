e5291bef7bea6b581a183e5864268ffa32f158e9
Nsukami Di Kiesse Patrick <ptrck@nskm.xyz>
2023-10-23 17:52:24 +0000

    WIP: Ensure foreign key, create tables during init proc

--- poc1/database.py
+++ poc1/database.py
@@ -11,45 +11,62 @@ database_path = basedir / "database.sqlite"
 @st.cache_resource
 def init_connection(database_path):
     con = sqlite3.connect(database_path, check_same_thread=False)
+    con.execute("PRAGMA foreign_keys = 1")
     # con.row_factory = dict_factory
     if "con" not in st.session_state:
         st.session_state.con = con
-    return con
 
-@st.cache_resource
-def create_plate_types_table():
-    con = st.session_state.con
+    # create one table plate_types
     with contextlib.closing(con.cursor()) as cur:
         cur.execute("""
         create table if not exists plate_types(
         eid integer primary key,
         type varchar(50) not null,
         nb_cols integer not null,
-        col_names varchar(50) not null, -- letters of numbers
         nb_rows integer not null,
-        row_names varchar(50) not null, -- letters of numbers
+
+        names_cols varchar(50) not null, -- letters of numbers
+        names_rows varchar(50) not null, -- letters of numbers
+
         nb_whites integer not null,
         nb_pos integer not null,
         nb_negs integer not null,
+
+        wells_whites text not null,
+        wells_pos text not null,
+        wells_negs text not null,
+
         nb_wells integer GENERATED ALWAYS AS (nb_cols * nb_rows),
         nb_patient_wells integer GENERATED ALWAYS AS ((nb_cols * nb_rows) - (nb_pos+nb_negs+nb_whites)),
+
+
         created varchar(50) not null
         );
         """)
         con.commit()
 
-@st.cache_resource
-def create_plates_table():
-    con = st.session_state.con
+    # create second table plate plans
     with contextlib.closing(con.cursor()) as cur:
         cur.execute("""
         create table if not exists plates(
         eid integer primary key,
-        type integer not null,
+        type_eid integer not null,
         status text check(status IN ('empty','filled','processed')) NOT NULL DEFAULT 'empty',
-        wells_for_whites text not null,
-        wells_for_pos text not null,
-        wells_for_negs text not null
+        FOREIGN KEY(type_eid) REFERENCES plate_types(eid)
+        );
+        """)
+        con.commit()
+
+    # create patients table
+    with contextlib.closing(con.cursor()) as cur:
+        cur.execute("""
+        create table if not exists patients(
+        eid integer primary key,
+        firstname varchar(50) not null,
+        lastname varchar(50) not null,
+        age integer not null,
+        sex varchar(50) not null,
+        created varchar(50) not null
         );
         """)
         con.commit()
@@ -57,8 +74,13 @@ def create_plates_table():
 
 def insert_plate_type(con, params):
     query = """
-    insert into plate_types (type, nb_cols, col_names, nb_rows, row_names, nb_whites, nb_pos, nb_negs, created)
-    values (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    insert into plate_types (
+    type, nb_cols, nb_rows,
+    names_cols, names_rows,
+    nb_whites, nb_pos, nb_negs,
+    wells_whites, wells_pos, wells_negs,
+    created)
+    values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """
     with contextlib.closing(con.cursor()) as cur:
         cur.execute(query, params)
