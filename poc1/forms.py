1f12cdc93ea673b901c5bf1cda71953a1e89ba52
Nsukami Di Kiesse Patrick <ptrck@nskm.xyz>
2023-10-23 17:53:07 +0000

    WIP: update form for adding types of PCR plate plans

--- poc1/forms.py
+++ poc1/forms.py
@@ -1,4 +1,14 @@
 import streamlit as st
+well_names = [
+    "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12",
+    "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12",
+    "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12",
+    "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12",
+    "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10", "E11", "E12",
+    "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",
+    "G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "G10", "G11", "G12",
+    "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12",
+]
 
 def plate_type_form():
     type_ = st.text_input("Type of the plate",
@@ -17,16 +27,16 @@ def plate_type_form():
                                 placeholder="Number of rows",
                                 label_visibility="collapsed")
     with col2:
-        col_names = st.selectbox("Columns naming scheme",
-                                          ("Letters", "Numbers"),
-                                          index=None,
-                                          placeholder="How to name the columns?",
-                                          label_visibility="collapsed")
-        row_names = st.selectbox("Rows naming scheme",
-                                          ("Letters", "Numbers"),
-                                          index=None,
-                                          placeholder="How to name the rows?",
-                                          label_visibility="collapsed")
+        names_cols = st.selectbox("Columns naming scheme",
+                                  ("Letters", "Numbers"),
+                                  index=None,
+                                  placeholder="How to name the columns?",
+                                  label_visibility="collapsed")
+        names_rows = st.selectbox("Rows naming scheme",
+                                  ("Letters", "Numbers"),
+                                  index=None,
+                                  placeholder="How to name the rows?",
+                                  label_visibility="collapsed")
 
 
     col4, col5 = st.columns([1,3])
@@ -47,21 +57,20 @@ def plate_type_form():
                                 placeholder="Number of negatives",
                                 label_visibility="collapsed")
         with col5:
-            options = ["foo", "bar", "baz", "corge"]
-            white_wells = st.multiselect("Location for whites",
-                                         options=options,
+            wells_whites = st.multiselect("Location for whites",
+                                         options=well_names,
                                          placeholder="Locations for whites",
                                          max_selections=nb_whites,
                                          label_visibility="collapsed")
 
-            pos_wells = st.multiselect("Location for positives",
-                                      options=options,
+            wells_pos = st.multiselect("Location for positives",
+                                      options=well_names,
                                       placeholder="Locations for positives",
                                       max_selections=nb_pos,
                                       label_visibility="collapsed")
-            neg_wells = st.multiselect("Location for negatives",
-                                      options=options,
+            wells_negs = st.multiselect("Location for negatives",
+                                      options=well_names,
                                       placeholder="Location for for negatives",
                                       max_selections=nb_negs,
                                       label_visibility="collapsed")
-    return type_, nb_cols, col_names, nb_rows, row_names, nb_whites, nb_pos, nb_negs
+    return type_, nb_cols, nb_rows, names_cols, names_rows, nb_whites, nb_pos, nb_negs
